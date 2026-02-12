e90a13871157e9425428ef81b6c743bba05c5248
vijayganesh91 <vijayganesh9121@gmail.com>
2026-02-12 23:25:57 +0530

    rebuild jobs api

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/auth.js";
 import jobsRoutes from "./routes/jobs.js";
 import applicationsRoutes from "./routes/applications.js";
